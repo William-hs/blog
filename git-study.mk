a15c2a943a6f7d72b8f13bc23dde03d12d8a8205
William-hs <2540219915@qq.com>
2021-03-16 23:27:53 +0800

    new2 file

--- git-study.mk
+++ git-study.mk
@@ -6,3 +6,13 @@ git分为三个区 工作区、暂存区、仓库
 从GitHub上拷贝仓库 git clone 地址
 查看状态 git status
 提交介绍 git commit -m “描述”
+上传到github的网络仓库 git push -u origin main(也就是该分支？）
+改文件 vim 文件名 i 开始编写 结束后ctrl+C 英文字符： wq退出
+#关联本地和网络仓库
+进入根目录 cd ~
+ssh-keygen -t rsa -b 4096 -C 邮箱
+出现公钥和私钥的地址
+cd进入地址
+cat 查看文件 公钥复制
+在github设置中绑定
+
