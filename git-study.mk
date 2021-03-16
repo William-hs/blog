f821ed2c16c8380d692f565648f722807801051d
William-hs <2540219915@qq.com>
2021-03-16 23:15:31 +0800

    new file

---
+++ git-study.mk
@@ -0,0 +1,8 @@
+#git的工作流程
+git分为三个区 工作区、暂存区、仓库
+编写在blog里的文件处于工作区（workplace）、当add该文件后，文件处于暂存区、commit后进入仓库，push进github
+#git的一些命令（commod）
+在本地新建仓库 git init
+从GitHub上拷贝仓库 git clone 地址
+查看状态 git status
+提交介绍 git commit -m “描述”
